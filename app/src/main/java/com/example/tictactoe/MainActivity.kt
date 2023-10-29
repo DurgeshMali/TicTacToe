c20a7d2ccd7ba2bb6de366f7bcfb87345fa4e5c6
Durgesh <durgeshmali5824@gmail.com>
2023-10-29 06:02:26 +0530

    tic

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.tictactoe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tictactoe.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    var binding: ActivityMainBinding? = null
+    private val combinationList: List<IntArray> = ArrayList()
+    private val boxPositions = intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0) //9 zero
+    private val playerTurn = 1
+    private val totalSelectedBoxes = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
